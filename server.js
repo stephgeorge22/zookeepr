2a97a5dc211cda9642f33c0f0a6eb2dd3c9462c8
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-04 18:13:40 -0400

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-const fs = require('fs');
-const path = require('path');
+
 
 const { animals } = require('./data/animals');
 
