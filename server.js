8bd984264cd82a14b06e46c354f412f00251724f
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-06 20:40:56 -0400

    serving front end

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const { animals } = require('./data/animals');
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -68,6 +69,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
       res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
